7b8554cdd5d21805288d93688a8cbec92793ba10
Adam Raymond Wright <awright3@stanford.edu>
2019-01-17 12:04:46 -0800

    added adam_photometry_wrapper.sh. This  should be a useful tool for photometry, REMEMBER TO USE IT

---
+++ adam_photometry_wrapper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -xv
+
+. MACS0429-02.ini
+. SUBARU.ini
+. progs.ini
+
+
+#./adam_do_photometry_final_starcat.sh ${cluster} ${detect_filter} ${lensing_filter} aper PHOTO 2>&1 | tee -a OUT-adam_do_photometry_${cluster}_PHOTO.log
+#exit_stat=$?
+#if [ "${exit_stat}" -gt "0" ]; then
+#        exit ${exit_stat};
+#fi
+#exit 0
+
+#./adam_do_photometry_final_starcat.sh ${cluster} ${detect_filter} ${lensing_filter} aper MERGE 2>&1 | tee -a OUT-adam_do_photometry_${cluster}_MERGE.log
+#exit_stat=$?
+#if [ "${exit_stat}" -gt "0" ]; then
+#        exit ${exit_stat};
+#fi
+
+#./adam_do_photometry_final_starcat.sh ${cluster} ${detect_filter} ${lensing_filter} aper STARS 2>&1 | tee -a OUT-adam_do_photometry_${cluster}_STARS.log
+#exit_stat=$?
+#if [ "${exit_stat}" -gt "0" ]; then
+#        exit ${exit_stat};
+#fi
+#exit 0
+
+./adam_do_photometry_final_starcat.sh ${cluster} ${detect_filter} ${lensing_filter} aper SDSS 2>&1 | tee -a OUT-adam_do_photometry_${cluster}_SDSSmode.log
+./adam_do_photometry_final_starcat.sh ${cluster} ${detect_filter} ${lensing_filter} aper SHAPES 2>&1 | tee -a OUT-adam_do_photometry_${cluster}_SHAPESmode.log
+exit_stat=$?
+if [ "${exit_stat}" -gt "0" ]; then
+        exit ${exit_stat};
+fi
+exit 0
+
+cd ~/ldaclensing/
+
+./adam_do_photometry_final_starcat.sh  ${detect_filter} ${lensing_filter} aper BIGMACSCALIB BIGMACSAPPLY 2>&1 | tee -a OUT-adam_do_photometry_${cluster}_bigmacs.log
+exit_stat=$?
+if [ "${exit_stat}" -gt "0" ]; then
+        exit ${exit_stat};
+fi
